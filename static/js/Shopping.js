70a7cb9c9422e0b185c9e83fe8e9ae71736f0955
Jiande Li <alexlijd@gmail.com>
2021-02-04 21:18:09 -0500

    ipload merge

--- static/js/Shopping.js
+++ static/js/Shopping.js
@@ -8,49 +8,54 @@ window.addEventListener('load', function () {
       xhr.open("GET", "/allProducts");
 		// xhr.setRequestHeader("Content-type", "application/json");
 		xhr.send();
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-        var res = xhr.responseText;
-        // var res = JSON.stringify(res);
-        var endIndex = res.length-3;
-        var sub = res.substring(31,endIndex);
-        var arr = sub.split("},");
-        var product = '<ul>';
-        console.log(res);
-        console.log(sub);
-         console.log(arr);
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var res = JSON.parse(xhr.responseText);
+      // // var res = JSON.stringify(res);
+      // var endIndex = res.length-3;
+      // var sub = res.substring(31,endIndex);
+      // var arr = sub.split("},");
+      // var product = '<ul>';
+      // console.log(res);
+      // console.log(sub);
+      //  console.log(arr);
 
-        var str = '';
-				// if (res.status == 'success') {
-        for (let i = 0; i < arr.length; i++){
-          var ar = arr[i].split(","); 
-          var a = ar[0].split(":"); 
-          var c = '';
+      let product = '<ul>';
+      // if (res.status == 'success') {
+      // for (let i = 0; i < arr.length; i++){
+      //   var ar = arr[i].split(","); 
+      //   var a = ar[0].split(":"); 
+      //   var c = '';
 
-          // if (ar.length > 4) {
-          //   for (let b = 3; b < ar.length; b++) { 
-          //     ar[2] += ar[b];
-          //   }
+      // if (ar.length > 4) {
+      //   for (let b = 3; b < ar.length; b++) { 
+      //     ar[2] += ar[b];
+      //   }
+      // }
+      res.forEach((item) => {
+        product += "<li><div class='small_box'><img src='" + item.itemImageUrl + " ' alt=''><p>" + item.itemName + "</p><div class='price'>$" + item.price + "<span>$" + (item.price + 100.00) + "</span><div class='sold'><span class='sold_percen'>Sold " + item.amount + "%</span><span class='bar'><div></div></span><span class='sold_percen'>" + (100 - item.amount) + " left</span></div><button class='buy_product'><a href='javascript:;'>Buy Now!</a></button></div></li>"
+      })
+
+
+          // console.log(ar);
+          // console.log(ar[3]);
+          // //console.log(ar[3].substring(17, ar[3].length-1));
+
+          // c = "<li><div class='small_box'><img src='" + ar[3].substring(18, ar[3].length-1) + " ' alt=''><p>" + ar[0].substring(15, ar[0].length - 1) + "</p><div class='price'>$" + ar[1].substring(9) + "<span>$" + (parseInt( ar[1].substring(9)) + 100) + "</span><div class='sold'><span class='sold_percen'>Sold " + ar[2].substring(10) + "%</span><span class='bar'><div></div></span><span class='sold_percen'>" + (100 - parseInt(ar[2].substring(10))) + " left</span></div><button class='buy_product'><a href='javascript:;'>Buy Now!</a></button></div></li>"
+          // product += c;
           // }
-
-
-          console.log(ar);
-          console.log(ar[3]);
-           console.log(ar[3].substring(17, ar[3].length-1));
-
-          c = "<li><div class='small_box'><img src='" + ar[3].substring(18, ar[3].length-1) + " ' alt=''><p>" + ar[0].substring(15, ar[0].length - 1) + "</p><div class='price'>$" + ar[1].substring(9) + "<span>$" + (parseInt( ar[1].substring(9)) + 100) + "</span><div class='sold'><span class='sold_percen'>Sold " + ar[2].substring(10) + "%</span><span class='bar'><div></div></span><span class='sold_percen'>" + (100 - parseInt(ar[2].substring(10))) + " left</span></div><button class='buy_product'><a href='javascript:;'>Buy Now!</a></button></div></li>"
-          product += c;
-          }
-        product += "</ul>";
-         console.log(product);
-        phone_box.innerHTML=product;
+         product += "</ul>";
+      console.log(product);
+      phone_box.innerHTML = product;
 					
-				// }
-				// else {
-				// 	alert(res);
-				// }
-			}
-		}
+      // }
+      // else {
+      // 	alert(res);
+      // }
+    }
+  }
+  
+		
       
         // $.ajax({
         //   type:"post",
@@ -100,7 +105,7 @@ window.addEventListener('load', function () {
 // >>>>>>> online
 
 
-  var phone_box = document.querySelector(".phone_box");
+ 
     
 // <<<<<<< HEAD
     // buy.onclick = function () {
@@ -123,17 +128,10 @@ window.addEventListener('load', function () {
       //     console.log(localStorage.getItem('count'));
       // }
         
-  let aaa = 'hh';
+ 
 
 
-  var clock = document.querySelector('.clock');
-
-  clock.onclick=function(){ 
-    aaa = 'ABCD';
-    console.log(aaa);
-  };
-
-  exports = {aaa}
+  
     
 // =======
 //   var xhr = new XMLHttpRequest();
